72c30d8792719ac095b187f11951832c93c9a79e
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-09 10:43:38 -0300

    sales-summary html and styles

---
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function SalesSummaryCard() {
+  return (
+    <div className="sales-summary-card base-card">
+      <div></div>
+    </div>
+  );
+}
+
+export default SalesSummaryCard;
