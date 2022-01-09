ccf07f014364c6a7db83a1ffd30f02886d69bee7
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-01-09 14:25:30 -0300

    sales-summary-card html and styles with mocked data

--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,9 +1,12 @@
 import './styles.css';
+import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
 
 function SalesSummaryCard() {
   return (
     <div className="sales-summary-card base-card">
-      <div></div>
+      <AvatarIcon />
+      <h3 className="sales-summary-card-value">534.00</h3>
+      <span className="sales-summary-card-label">Média</span>
     </div>
   );
 }
